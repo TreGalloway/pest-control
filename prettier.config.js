f960a660d6089b07e64d621ebcbeeb6276083a08
Tre Galloway <88118603+TreGalloway@users.noreply.github.com>
2023-08-17 10:57:33 -0500

    Removed nav component, added hero , setup home

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    semi: false,
+    plugins: [require('prettier-plugin-tailwindcss')],
+    useTabs: true,
+	trailingComma: "none",
+	printWidth: 100
+  }
+  
